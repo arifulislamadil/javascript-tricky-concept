15ceb5592e9753a4f7d629f1a8bb2d485515a20f
Arifu Islam Adil <adil.islam.arif@gmail.com>
2022-01-13 18:43:08 +0600

    call back in javascript

---
+++ call-back.js
@@ -0,0 +1,11 @@
+function welcomeMessage(name) {
+    console.log(name);
+}
+
+// const name = ["Tom Hanks", "Tom Boddu", "Tom Crouse"];
+// const myObject = { name: "Tom Hanks", age: 22 }
+
+function greadMorning() {
+    console.log("Good Morning");
+}
+welcomeMessage(greadMorning)
